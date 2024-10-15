015be52fd4f0d0de597c73f6f723efe93909d707
Omar Mekky <omar@payrails.com>
2024-10-15 11:41:56 +0300

    Add telescope-undo

---
+++ arch-configs/nvim/lua/plugins/telescope-undo.lua
@@ -0,0 +1,23 @@
+return {
+  "debugloop/telescope-undo.nvim",
+
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+
+  keys = {
+    {
+      "<leader>uu",
+      "<cmd>Telescope undo<cr>",
+      desc = "Coverage Toggle",
+    },
+  },
+  config = function()
+    LazyVim.on_load("telescope.nvim", function()
+      local ok, err = pcall(require("telescope").load_extension, "undo")
+      if not ok then
+        LazyVim.error("Failed to load `telescope-undo.nvim`:\n" .. err)
+      end
+    end)
+  end,
+}
