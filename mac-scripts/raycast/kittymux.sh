93690634c88b562d501116801469048cc25e9754
Omar Mekky <iam@cousine.me>
2025-04-03 22:50:00 +0200

    Cleanup tmux config
    - fix tmux theme source-file order not loading variables on new-session
    - add raycast custom command to open kitty with a tmux session

---
+++ mac-scripts/raycast/kittymux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title KittyMux
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ⌨️
+# @raycast.argument1 { "type": "text", "placeholder": "session name", "optional": true }
+# @raycast.currentDirectoryPath ~
+
+# Documentation:
+# @raycast.description Runs a Kitty terminal with a tmux session
+# @raycast.author Omar Mekky
+# @raycast.authorURL https://cousine.me
+
+SESSION_NAME="main"
+
+if [[ -n $1 ]]; then
+  SESSION_NAME=$1
+fi
+
+kitty --start-as=fullscreen tmux new-session -A -s "cousine-$SESSION_NAME"
